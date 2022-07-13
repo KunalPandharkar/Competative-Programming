2a7e890951579a7d238f7312f7feba572da19761
Kunal Pandharkar <63575815+KunalPandharkar@users.noreply.github.com>
2022-07-13 10:09:19 +0530

    Decimal To Binary Conversion
    Decimal To Binary Conversion

---
+++ DecimalToBinary.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// decimal tp binary
+
+string decimalTobinary(int n)
+{
+    string binary;
+    while (n != 0)
+    {
+        binary.push_back((char)(n % 2) + '0');
+        n = n / 2;
+    }
+
+    reverse(binary.begin(), binary.end());
+
+    return binary;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    // using inbuilt bitset container
+    // string binary = bitset<32>(n).to_string();
+    // int find = binary.find('1');
+    // if(find != string::npos)
+    // binary.erase(binary.begin(),binary.begin() + find);
+    // cout << binary << endl;
+
+    cout << decimalTobinary(n);
+}
