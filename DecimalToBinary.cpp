67425d862165626399a5506ef7e98b4d6e2cad0f
Kunal Pandharkar <63575815+KunalPandharkar@users.noreply.github.com>
2022-07-13 12:21:07 +0530

    Add files via upload

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 // decimal tp binary
 
-string decimalTobinary(int n)
-{
-    string binary;
-    while (n != 0)
-    {
-        binary.push_back((char)(n % 2) + '0');
-        n = n / 2;
-    }
+// string decimalTobinary(int n)
+// {
+//     string binary;
+//     while (n != 0)
+//     {
+//         binary.push_back((char)(n % 2) + '0');
+//         n = n / 2;
+//     }
 
-    reverse(binary.begin(), binary.end());
+//     reverse(binary.begin(), binary.end());
 
-    return binary;
-}
+//     return binary;
+// }
 
 int main()
 {
@@ -29,15 +29,18 @@ int main()
     // binary.erase(binary.begin(),binary.begin() + find);
     // cout << binary << endl;
 
-    // using bitwise &
-    // string binary;
-    // while (n > 0)
-    // {
-    //     binary.push_back((char)(n & 1) + '0');
-    //     n = n >> 1;
-    // }
-    // reverse(binary.begin(), binary.end());
-    // cout << binary;
+    //using bitwise &
+    int binary = 0;
+    int count = 0;
+    while (n > -1) // 1 0 1
+    {
+        int dig = n & 1; // 1 0 1
+        binary = (dig * pow(10,count)) + binary; 
 
-    cout << decimalTobinary(n);
+        n = n >> 1;
+        count++;
+    }
+    cout << binary;
+
+    //cout << decimalTobinary(n);
 }
